2c85a8473fe6cc227f398eced4d93047b7cc8dbb
David G. Simmons <santafen@mac.com>
2024-04-15 16:30:27 -0400

    fix payment

--- src/components/StripePricing.tsx
+++ src/components/StripePricing.tsx
@@ -1,11 +1,9 @@
 function stripeEmbed() {
   return {
     __html: `<script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-    <stripe-pricing-table
-    pricing-table-id="prctbl_1OgV8HGuKQxVPasTQ9Cm8EPf"
-          publishable-key="pk_test_51OYEejGuKQxVPasTmIP0YpYi6bMc5YxPdbTODK6FO0quQ9clYbr9TC9Kihv3o2zV8ErBY2xRD4OwnLNoxgE265B600yqy7eDkN"
-    >
-    </stripe-pricing-table>`,
+<stripe-pricing-table pricing-table-id="prctbl_1P5w8kGuKQxVPasTTCV3iCrb"
+publishable-key="pk_live_51OYEejGuKQxVPasTx38tvhF18bkVUopxhtaqFB1HwNd89om7n7GYofFpEKnMGMSZ92rbVeFMwETMjLBXQJr6uR8i00eVzQL3rn">
+</stripe-pricing-table>`,
   };
 }
 
